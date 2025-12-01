f484685a509134cf417872082a2cb7ed8b6175df
sakshampanwar101 <sakshampanwar444@gmail.com>
2025-12-01 20:39:40 +0530

    Fix CORS for vercel

--- foodcourt/app.js
+++ foodcourt/app.js
@@ -13,7 +13,7 @@ const rtsIndex = require('./routes/index.router');
 var app = express();
 
 app.use(cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: [process.env.CLIENT_URL, 'http://localhost:5173'],
     credentials: true
 }));
 
